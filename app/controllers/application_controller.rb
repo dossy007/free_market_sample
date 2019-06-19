d5d73f7fe3bfaa19d79c17bc55b508c3a35256e3
dossy007 <fedox.toshi@gmail.com>
2019-06-19 16:11:06 +0900

    add production_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   private
+
+  def production?
+    Rails.env.production?
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == "free" && passeord == "market"
