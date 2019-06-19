c21a91115aa1cf0a79886a9641981125b78c23bf
dossy007 <fedox.toshi@gmail.com>
2019-06-19 14:37:12 +0900

    Edit elastic ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server "13.114.183",
+server "13.114.183.139",
   user: "ec2-user",
   roles: %w{web db app}
 #   ssh_options: {
