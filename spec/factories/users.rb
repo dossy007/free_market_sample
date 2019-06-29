cdb545bdd17ed8ac519cdc2e38835359e347aaaf
dossy007 <fedox.toshi@gmail.com>
2019-06-29 20:02:33 +0900

    Edit factories/user.rb by faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,15 +3,15 @@ FactoryBot.define do
   factory :user do
     nickname              {"abe"}
     email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
-    last_name	{"比嘉"}
-    first_name	{"将吾"}
+    password = Faker::Internet.password(6)
+    password_confirmation = Faker::Internet.password(8)
+    last_name {Faker::Name.last_name}
+    first_name {Faker::Name.first_name}
     last_kana	{"ひが"}
     first_kana	{"しょうご"}
-    postal_code	{"664-0858"}
+    postal_code {Faker::Address::postcode}
     prefecture_id	{"0"}
-    city_name	{"札幌市"}
+    city_name	{Faker::Address::city}
     building_name	{"札幌1-1-1"}
     house_number	{"比嘉コーポ101"}
   end
