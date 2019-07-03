efac7ba4d552412717c7e79858160355b2f0cd42
dossy007 <fedox.toshi@gmail.com>
2019-07-03 15:03:59 +0900

    Add model find_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,41 @@ class User < ApplicationRecord
   belongs_to :user, optional: true
   extend ActiveHash::Associations::ActiveRecordExtensions
    belongs_to_active_hash :prefecture
+
+
+   def self.find_oauth(auth)
+   	uid = auth.uid
+   	provider = auth.provider
+   	snscredential = SnsCredential.where(uid: uid, provider: provider).first
+
+	   	if snscredentials.present? #sns登録のみ完了してるuser
+	   		user = User.where(id: snscredential.user_id).first
+	   		unless user.present?
+	   			user = User.new(
+	   				nickname: auth.info.name,
+	   				email: auth.info.email
+	   				)
+	   		end
+	   		sns = snscredential
+	   	else #sns未登録
+	   		user = User.where(email: auth.info.email).first
+	   		if user.present?
+	   			sns = SnsCredential.new(
+	   				uid: uid,
+	   				provider: provider,
+	   				user_id: user.id
+	   				)
+	   		else
+	   			user = User.new(
+	   				nickname: auth.info.name,
+	   				email: auth.info.email
+	   				)
+	   			sns = SnsCredential.create(
+	   				uid: uid,
+	   				provider: provider
+	   				)
+	   		end
+	    end
+	    return {user: user, sns_id: sns.id}
+	end
 end
