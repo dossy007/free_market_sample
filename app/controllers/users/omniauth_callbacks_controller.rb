27ce125b3e26f916ae179b035845802f468f0408
dossy007 <fedox.toshi@gmail.com>
2019-07-03 15:04:26 +0900

    Add callback action for facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,6 +7,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should also create an action method in this controller like this:
   # def twitter
   # end
+  def facebook
+    callback_for(:facebook)
+  end
+
+  def callback_for(provider)
+    info = User.find_oauth(request.env["omniauth.auth"])
+    @user = info[:user]
+    sns_id = info[:sns_id]
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else
+      session["devise.sns_id"] = sns_id
+      render template: "devise/registrations/new"
+    end
+
+    def failure
+      redirect_to root_path and return
+    end
+  end
 
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
