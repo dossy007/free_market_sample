e2be4a152838c4345ac4e404e45238bcef40436d
dossy007 <fedox.toshi@gmail.com>
2019-07-04 16:16:48 +0900

    edit create action for registration_cotroller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    super
+    if params[:user][:password] == "" #sns登録なら
+      params[:user][:password] = "Devise.friendly_token.first(6)" #
+      params[:user][:password_confirmation] = "Devise.friendly_token.first(6)"
+      super
+      sns = SnsCredential.update(user_id: @user.id)
+    else
+      super
+    end
   end
 
   # GET /resource/edit
