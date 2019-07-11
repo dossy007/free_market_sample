3c88036f01b9222684265c03d9efd080d86fe3cd
dossy007 <fedox.toshi@gmail.com>
2019-07-11 21:56:33 +0900

    Edit indent

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,45 +9,44 @@ class User < ApplicationRecord
 
   ##Assosiation
     has_many :sns_credentials, ->  { denpendent (:destroy) }
-
-  belongs_to :user, optional: true
+  	belongs_to :user, optional: true
   extend ActiveHash::Associations::ActiveRecordExtensions
-   belongs_to_active_hash :prefecture
+  	belongs_to_active_hash :prefecture
 
 
-   def self.find_oauth(auth)
-   	uid = auth.uid
-   	provider = auth.provider
-   	snscredentials = SnsCredential.where(uid: uid, provider: provider).first
+	def self.find_oauth(auth)
+		uid = auth.uid
+		provider = auth.provider
+		snscredentials = SnsCredential.where(uid: uid, provider: provider).first
 
-	   	if snscredentials.present? #sns登録のみ完了してるuser
-	   		user = User.where(id: snscredential.user_id).first
-	   		unless user.present?
-	   			user = User.new(
-	   				nickname: auth.info.name,
-	   				email: auth.info.email
-	   				)
-	   		end
-	   		sns = snscredential
-	   	else #sns未登録
-	   		user = User.where(email: auth.info.email).first
-	   		if user.present?
-	   			sns = SnsCredential.new(
-	   				uid: uid,
-	   				provider: provider,
-	   				user_id: user.id
-	   				)
-	   		else
-	   			user = User.new(
-	   				nickname: auth.info.name,
-	   				email: auth.info.email
-	   				)
-	   			sns = SnsCredential.create(
-	   				uid: uid,
-	   				provider: provider
-	   				)
-	   		end
-	    end
-	    return {user: user, sns_id: sns.id}
+	 	if snscredentials.present? #sns登録のみ完了してるuser
+	 		user = User.where(id: snscredential.user_id).first
+	 		unless user.present?
+	 			user = User.new(
+	 				nickname: auth.info.name,
+	 				email: auth.info.email
+	 				)
+	 		end
+	 		sns = snscredential
+	 	else #sns未登録
+	 		user = User.where(email: auth.info.email).first
+	 		if user.present?
+	 			sns = SnsCredential.new(
+	 				uid: uid,
+	 				provider: provider,
+	 				user_id: user.id
+	 				)
+	 		else
+	 			user = User.new(
+	 				nickname: auth.info.name,
+	 				email: auth.info.email
+	 				)
+	 			sns = SnsCredential.create(
+	 				uid: uid,
+	 				provider: provider
+	 				)
+	 		end
+	  end
+	  return {user: user, sns_id: sns.id}
 	end
 end
