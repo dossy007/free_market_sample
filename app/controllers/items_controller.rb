a3548a120f8e226436e894a74a91ee09a0419f0a
dossy007 <fedox.toshi@gmail.com>
2019-07-14 23:18:30 +0900

    Add action for items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,20 @@
 class ItemsController < ApplicationController
+	before_action :authenticate_user!,only: [:new,:create]
+
   def index
     @items = Item.limit(10)
+  end
+
+  def new
+  	@item = Item.new
+  end
+
+  def create
+  	@item = Item.create
+  end
+
+
+  private
+  def param
   end
 end
