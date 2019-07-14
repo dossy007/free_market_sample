0ee5624df2b47756989c101bd45e14ca57dad6f7
dossy007 <fedox.toshi@gmail.com>
2019-07-14 13:22:18 +0900

    Edit ssh config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -42,9 +42,10 @@
 # Global options
 # --------------
  set :ssh_options, {
-   keys: %w(/home/rlisowski/.ssh/id_rsa),
-   forward_agent: false,
-   auth_methods: %w(password)
+ 	config: false
+   # keys: %w(/home/rlisowski/.ssh/id_rsa),
+   # forward_agent: false,
+   # auth_methods: %w(password)
  }
 #
 # The server-based syntax can be used to override options:
