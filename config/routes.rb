58d3d9a29cddc0db1fb3ad7aa731e2572b6aa2e8
dossy007 <fedox.toshi@gmail.com>
2019-07-18 14:23:31 +0900

    Edit items do image routeing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
-  resources :items
+  resources :items do
+  	resources :images
+  end
   resources :users
 end
