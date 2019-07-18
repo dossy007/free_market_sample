8e04e9691c75329e47b8b40ee5ac978369b2daac
dossy007 <fedox.toshi@gmail.com>
2019-07-18 14:18:58 +0900

    Edit Item.limit(8)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 	before_action :authenticate_user!,only: [:new,:create]
 
   def index
-    @items = Item.limit(10)
+    @items = Item.limit(8)
   end
 
   def new
