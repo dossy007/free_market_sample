be9c8e208f751117b933985b92b48569dbf79e2d
dossy007 <fedox.toshi@gmail.com>
2019-08-04 20:35:14 +0900

    Edit items_controller create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,14 +7,31 @@ class ItemsController < ApplicationController
 
   def new
   	@item = Item.new
-  	@item.images.build
     @item.images.build
   end
+
+  def show
+  	@item = Item.find(params[:id])
+  end
+
+  def edit
+  	@item = Item.find(params[:id])
   end
 
   def create
-  	item = Item.new(item_params)
-  	item.save
+  	@item = Item.new(item_params)
+  	# respond_to do |format|
+       @item.save
+      	 #imagesを配列で受け取っているので、一枚一枚分けて保存
+          # params[:images][:image].each do |image|
+          #   @item.images.create(image: image,item_id: @item.id)
+          # end
+        redirect_to root_path
+      # else
+        # item_images.build
+        # format.html{render action: 'new'}
+      # end
+    # end
   end
 
 
