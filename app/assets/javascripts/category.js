b3aeae8ed073b26b648864b1458a2747634ed0ea
dossy007 <fedox.toshi@gmail.com>
2019-08-09 19:57:46 +0900

    Add Ajax for category

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,13 +1,38 @@
 $(document).on('turbolinks:load', function() {
-// jqをpage推移してきた時に動かす
+	var cat_id = $("#category")
+
+	function appendOption(num) {
+		if (num === 1){
+			var option_id = "middle_category"
+		}else if (num === 2) {
+			var option_id = "sub_category"
+		}
+		var html =
+		`<select name="item[category_id]" class="inputField inputField__select" id=${option_id} required="required">
+				<option value>---</option>
+		 </select>`
+		 cat_id.append(html)
+	}
+
+	function appendCategory(category, num) {
+		if(num == 1) {
+			var append_id = $("#middle_category")
+		}else if (num == 2) {
+			var append_id = $("#sub_category")
+		}
+		append_id.append(
+			$("<option>")
+			.val($(category).attr("id"))
+			.text($(category).attr("name"))
+		)
+	}
 
 	$("#item_category_id").on("change",function() {
 		var value_id = $(this).val()
-		console.log(value_id)
-
+		$("#middle_category").remove()
+		$("#sub_category").remove()
 		// ajaxをajaxをajaxを空じゃない時に使用
 		if(value_id !== ""){
-			console.log("からではない")
 			$.ajax({
 				type: "GET",
 				url:"/items/search",
@@ -16,9 +41,35 @@ $(document).on('turbolinks:load', function() {
 			})
 
 			.done(function(middle_category) {
-				console.log(middle_category)
+				appendOption(1)
+				middle_category.forEach(function(middle) {
+					appendCategory(middle, 1)
+				})
 			})
 		}
+	})
+
+//動的に追加したhtmlはdocument.onで指定すれば取得できる
+	$(document).on("change","#middle_category",function() {
+		console.log("11")
+		middle_c = $(this).val()
+		$("#sub_category").remove()
+		console.log(middle_c)
+
+		$.ajax({
+			type: "GET",
+			url: "/items/search",
+			data: {middle_id: middle_c},
+			dataType: "json"
+		})
+		.done(function(sub_category) {
+			console.log(sub_category)
+			appendOption(2)
+			sub_category.forEach(function(sub){
+				appendCategory(sub,2)
+			})
+
+		})
 	})
 
 })
