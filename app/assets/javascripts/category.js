2bf7e8d40a461158f4f176b78e73d70b2b38d19e
dossy007 <fedox.toshi@gmail.com>
2019-08-09 12:52:06 +0900

    Add ajax

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -2,11 +2,22 @@ $(document).on('turbolinks:load', function() {
 // jqをpage推移してきた時に動かす
 
 	$("#item_category_id").on("change",function() {
-		console.log("11")
-		var tt =$(this).val()
-		console.log(tt)
-		if(tt === ""){
-			console.log("からやで")
+		var value_id = $(this).val()
+		console.log(value_id)
+
+		// ajaxをajaxをajaxを空じゃない時に使用
+		if(value_id !== ""){
+			console.log("からではない")
+			$.ajax({
+				type: "GET",
+				url:"/items/search",
+				data:{value_id: value_id},
+				dataType: "json"
+			})
+
+			.done(function(middle_category) {
+				console.log(middle_category)
+			})
 		}
 	})
 
