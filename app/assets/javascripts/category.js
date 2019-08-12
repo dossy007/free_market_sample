16f9f48501178dff9ada27c8cefd59a443ad74c1
dossy007 <fedox.toshi@gmail.com>
2019-08-12 16:03:50 +0900

    Edit indent

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,31 +1,31 @@
   $(document).on('turbolinks:load', function() {
-		var cat_id = $("#category")
+    var cat_id = $("#category")
 
-		function appendOption(num) {
-			if (num === 1){
-				var option_id = "middle_category"
-			}else if (num === 2) {
-				var option_id = "sub_category"
-			}
-			var html =
-			`<select name="item[category_id]" class="inputField inputField__select" id=${option_id} required="required">
-					<option value>---</option>
-			 </select>`
-			 cat_id.append(html)
+    function appendOption(num) {
+    if (num === 1){
+    	var option_id = "middle_category"
+    }else if (num === 2) {
+    	var option_id = "sub_category"
+    }
+    var html =
+		`<select name="item[category_id]" class="inputField inputField__select" id=${option_id} required="required">
+				<option value>---</option>
+		 </select>`
+		 cat_id.append(html)
 		}
 
-		function appendCategory(category, num) {
-			if(num == 1) {
-				var append_id = $("#middle_category")
-			}else if (num == 2) {
-				var append_id = $("#sub_category")
-			}
-			append_id.append(
-				$("<option>")
-				.val($(category).attr("id"))
-				.text($(category).attr("name"))
-			)
-	  }
+	function appendCategory(category, num) {
+		if(num == 1) {
+			var append_id = $("#middle_category")
+		}else if (num == 2) {
+			var append_id = $("#sub_category")
+		}
+		append_id.append(
+			$("<option>")
+			.val($(category).attr("id"))
+			.text($(category).attr("name"))
+		)
+	}
 
 	$("#item_category_id").on("change",function() {
 		var value_id = $(this).val()
