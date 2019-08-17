a85f62da9c464020a2021eedcb2dc9d63bb1319a
dossy007 <fedox.toshi@gmail.com>
2019-08-17 22:44:31 +0900

    Edit indent

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,25 +8,26 @@ class CardsController < ApplicationController
 
   def create
     if current_user.card.present?
-		card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-		customer = Payjp::Customer.retrieve(card.customer_id)
-  		customer.delete
-  		card.delete
+		  card = Card.where(user_id: current_user.id).first
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
     end
-  	Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  	if params['payjp-token'].blank?
-  		redirect_to action: new
-  	else
-  		customer = Payjp::Customer.create(
-  			description: 'テスト',
-  			email: current_user.email,
-  			card: params['payjp-token'],
-  			metadata: {user_id: current_user.id}
-  			)
-  		@card = Card.new(user_id: current_user.id,customer_id: customer.id, card_id: customer.default_card)
-  		@card.save
-  		redirect_to action: "new"
-  	end
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to action: new
+    else
+      customer = Payjp::Customer.create(
+      description: 'テスト',
+      email: current_user.email,
+      card: params['payjp-token'],
+      metadata: {user_id: current_user.id}
+      )
+      @card = Card.new(user_id: current_user.id,customer_id: customer.id, card_id: customer.default_card)
+      @card.save
+      redirect_to action: "new"
+    end
   end
 end
