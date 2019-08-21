0f1673ba44eccf4a7dfdab27ce6f024a5b4a3f41
dossy007 <fedox.toshi@gmail.com>
2019-08-21 20:26:20 +0900

    Edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,15 @@ class User < ApplicationRecord
 
   ##Assosiation
     has_many :sns_credentials, ->  { denpendent (:destroy) }
-  	belongs_to :user, optional: true
+  	# belongs_to :user, optional: true
     has_one :card
-    has_many :deals_of_seller, :class_name => 'Deal', :foreign_key =>'seller_id'
-		has_many :deals_of_buyer, :class_name => 'Deal', :foreign_key =>'buyer_id'
-		has_many :items_of_seller, :through => :deals_of_seller, :source =>'item'
-		has_many :items_of_buyer, :through => :deals_of_buyer, :source => 'item'
+  #   has_many :dealsell, :class_name => 'Deal', :foreign_key =>'seller_id'
+		# has_many :deals_of_buyer, :class_name => 'Deal', :foreign_key =>'buyer_id'
+		# has_many :items_of_seller, :through => :deals_of_seller, :source =>'item'
+		# has_many :items_of_buyer, :through => :deals_of_buyer, :source => 'item'
+
+		has_many :items, through: :sell
+		has_many :items, through: :buy
   extend ActiveHash::Associations::ActiveRecordExtensions
   	belongs_to_active_hash :prefecture
 
