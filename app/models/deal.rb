7980fcf12a3be53dc39979c43f2a052904a4b55a
dossy007 <fedox.toshi@gmail.com>
2019-08-21 20:25:43 +0900

    Delete deal.rb

--- app/models/deal.rb
+++
@@ -1,7 +0,0 @@
-class Deal < ApplicationRecord
-	belongs_to :seller, :class_name=>'User'
-	belongs_to :buyer, :class_name=>'User'
-	belongs_to :item
-	has_many :items_of_seller, :through => :deals_of_seller
-	#自分自信を参照するassociation
-end
