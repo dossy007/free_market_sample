6861b12eb929e1a26a27f443c85a6a894fb398e6
dossy007 <fedox.toshi@gmail.com>
2019-08-22 15:12:34 +0900

    Add redirect_flash

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,9 @@
 class CardsController < ApplicationController
+  before_action :redirect_flash, only: [:pay]
   require "payjp"
 
   def new
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
     if card != nil
@@ -36,5 +38,29 @@ class CardsController < ApplicationController
   end
 
   def pay
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      customer = current_user.card.customer_id
+      Payjp::Charge.create(
+        amount: params[:price],
+        customer: customer,
+        currency: 'jpy')
+      buy = Buy.create(buy_params)
+      buy.save
+      redirect_to root_path
+  end
+
+  private
+  def buy_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def redirect_flash
+    #自分のものは買えない&不正防止&カード登録
+    item = Item.find(params[:item_id])
+    if item.price != params[:price].to_i || current_user.id == item.sell.first.user_id || item.buy.present?
+      redirect_to root_path,flash: {alert: "不正もしくは売り切れもしくは自分で買おうとしてまへんか??"}
+    elsif current_user.card.blank?
+      redirect_to new_item_card_path, flash: {alert: "カードを登録してください"}
+    end
   end
 end
