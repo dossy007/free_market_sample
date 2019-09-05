eeb55feaf90c0507beb1cccf4b897f6dcd266754
dossy007 <fedox.toshi@gmail.com>
2019-09-05 17:30:29 +0900

    Add s3 setting for carrerwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,3 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^\x01-\x7E]/
+
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+    region: 'ap-northeast-1' #例 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'dossy3'
+  config.asset_host = 'https://s3-ap-northeast-1).amazonaws.com/dossy3'
+end
