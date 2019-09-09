95d574a49a968a1b69d7dacd1ab1136b20855906
dossy007 <fedox.toshi@gmail.com>
2019-09-09 09:23:07 +0900

    Change assosiation has_one
    sellとbuyの関係性を調整。
    1対1でこと足りると思った。
    Itemは一回しか、出品 or 購入しないから

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   has_many :images
 	has_many :users, through: :sell
 	has_many :users, through: :buy
-	has_many :sell
-	has_many :buy
+	has_one :sell
+	has_one :buy
   accepts_nested_attributes_for :images, allow_destroy: true
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :prefecture
