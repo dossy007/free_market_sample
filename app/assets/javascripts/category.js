5251b4cd1d6b1f514a0e3c331f96b1bf06d83541
dossy007 <fedox.toshi@gmail.com>
2019-09-18 14:35:29 +0900

    Add category.js
    検索用のcategoryはrequire:trueが不要なため

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -66,4 +66,76 @@
 			})
 		})
 	})
+
+
+  //検索画面のcategoryはrequire:trueを外す
+  var cat_id = $("#category")
+
+    function appendOp(num) {
+    if (num === 1){
+      var option_id = "m_category"
+    }else if (num === 2) {
+      var option_id = "s_category"
+    }
+    var html =
+    `<select name="item[category_id]" class="inputField inputField__select" id=${option_id}>
+        <option value>---</option>
+     </select>`
+     cat_id.append(html)
+      console.log(cat_id)
+    }
+
+  function appendC(category, num) {
+    if(num == 1) {
+      var append_id = $("#m_category")
+    }else if (num == 2) {
+      var append_id = $("#s_category")
+    }
+    append_id.append(
+      $("<option>")
+      .val($(category).attr("id"))
+      .text($(category).attr("name"))
+      )
+  }
+
+
+
+
+$("#category_id").on("change",function() {
+    var value_id = $(this).val()
+    $("#m_category").remove()
+    $("#s_category").remove()
+    // value_idが空ではない時にajazを使用
+    if(value_id !== ""){
+      $.ajax({
+        type: "GET",
+        url:"/items/search",
+        data:{value_id: value_id},
+        dataType: "json"
+      })
+      .done(function(middle_category) {
+        appendOp(1)
+        middle_category.forEach(function(middle) {
+          appendC(middle, 1)
+        })
+      })
+    }
+  })
+
+$(document).on("change","#m_category",function() {
+    middle_c = $(this).val()
+    $("#s_category").remove()
+    $.ajax({
+      type: "GET",
+      url: "/items/search",
+      data: {middle_id: middle_c},
+      dataType: "json"
+    })
+    .done(function(sub_category) {
+      appendOp(2)
+      sub_category.forEach(function(sub){
+        appendC(sub,2)
+      })
+    })
+  })
 })
