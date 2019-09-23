75f9083ddfb71645110f67384bd2836b460a6df7
dossy007 <fedox.toshi@gmail.com>
2019-09-23 12:47:34 +0900

    Delete commentout

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -23,31 +23,6 @@ crumb :category do |category|
 	parent :all_category
 end
 
-# crumb :parents do |category|
-# 	# binding.pry
-# 	# link "#{category}", category_item_path(category)
-# 	link "",category_item_path
-# 	parent :items
-# end
-# crumb :middle do |m|
-# 	link ""
-# 	parent :parents
-# 	end
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
 # files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
