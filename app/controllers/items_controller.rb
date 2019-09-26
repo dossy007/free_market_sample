800cd32c1f2f708006eb39540cf4728d8d6eb823
dossy007 <fedox.toshi@gmail.com>
2019-09-26 22:38:34 +0900

    Update memo

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -157,13 +157,13 @@ private
     m = params[:q][:mcategory_id]
     g = params[:q][:scategory_id]
     if base.length != 0
-      if g == nil || g == "" #grandchildを探す
-        if m.length != 0
-          #mがある時
-          base = Category.find(m).children.ids
-        else
-          #mがない時
+      if g == nil || g == "" #//MEMO: grandchildを探す
+        if m == nil || m == ""
+          #//MEMO: mがない時
           base = find_child_grand(base)
+        else
+          #//MEMO: mがある時
+          base = Category.find(m).children.ids
         end
       else
         base = g
